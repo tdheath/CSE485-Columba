4e470f6359d67b593ff94b7f9390456f05cb9367
IanPlu <personaltroll@gmail.com>
2013-10-30 21:34:05 -0700

    First real commit
    Garth + Tracey's starting code. Basic layout of components, framework
    for draggable blocks.

---
+++ CapGUI/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CapGUI
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        // Global variables used to keep track of the 
+        // mouse position and whether the object is captured
+        // by the mouse.
+        bool isMouseCaptured;
+        double mouseVerticalPosition;
+        double mouseHorizontalPosition;
+
+        public void Handle_MouseDown(object sender, MouseEventArgs args)
+        {
+            Canvas item = sender as Canvas;
+            mouseVerticalPosition = args.GetPosition(null).Y;
+            mouseHorizontalPosition = args.GetPosition(null).X;
+            isMouseCaptured = true;
+            item.CaptureMouse();
+        }
+
+        public void Handle_MouseMove(object sender, MouseEventArgs args)
+        {
+            Canvas item = sender as Canvas;
+            if (isMouseCaptured)
+            {
+
+                // Calculate the current position of the object.
+                double deltaV = args.GetPosition(null).Y - mouseVerticalPosition;
+                double deltaH = args.GetPosition(null).X - mouseHorizontalPosition;
+                double newTop = deltaV + (double)item.GetValue(Canvas.TopProperty);
+                double newLeft = deltaH + (double)item.GetValue(Canvas.LeftProperty);
+
+                // Set new position of object.
+                item.SetValue(Canvas.TopProperty, newTop);
+                item.SetValue(Canvas.LeftProperty, newLeft);
+
+                // Update position global variables.
+                mouseVerticalPosition = args.GetPosition(null).Y;
+                mouseHorizontalPosition = args.GetPosition(null).X;
+            }
+        }
+
+        public void Handle_MouseUp(object sender, MouseEventArgs args)
+        {
+            Canvas item = sender as Canvas;
+            isMouseCaptured = false;
+            item.ReleaseMouseCapture();
+            mouseVerticalPosition = -1;
+            mouseHorizontalPosition = -1;
+        }
+    }
+}
