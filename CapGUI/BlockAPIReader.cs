796f01ffef3412de4dc97355d6a6e0153675ea02
IanPlu <personaltroll@gmail.com>
2013-11-01 15:13:47 -0700

    Basic Block, BlockAPIReader, and BlockControl
    Added basic (not fully implemented) classes for a Block, the
    BlockAPIReader that generates block definitions, and the BlockControl to
    define the look of a block.

---
+++ CapGUI/BlockAPIReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CapGUI
+{
+    /// <summary>
+    /// Reads in blocks from the API file provided (XML, etc.) and creates lists of block definitions in our system
+    /// </summary>
+    public static class BlockAPIReader
+    {
+        /// <summary>
+        /// Reads in blocks from an API file and returns a list of these blocks. 
+        /// </summary>
+        /// <returns></returns>
+        public List<Block> readBlockDefinitions()
+        {
+            List<Block> blockList = new List<Block>();
+
+            //Iterate through and parse XML etc. here
+            /*
+             * foreach XML node, 
+             * create a block 
+             * foreach field specified by the XML
+             * add field to block
+             * finally, add block to blockList
+            */
+
+            return blockList;
+        }
+    }
+}
---
+++ CapGUI/BlockControl.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CapGUI
+{
+    /// <summary>
+    /// Controls the look and feel of a generic block, and handles rendering its contents (prompts, internal blocks, etc.)
+    /// </summary>
+    public partial class BlockControl : UserControl
+    {
+
+        Block block;
+        int numFields;
+        Color blockColor;
+
+        public BlockControl(Block newBlock)
+        {
+            InitializeComponent();
+            block = newBlock;
+
+            //Set up basic look and feel-- get color from block, define rectangle
+            blockColor = block.blockColor;
+            //
+
+            //Collect the number of fields in the block & partition space equally according to number of fields
+            numFields = block.typeFieldList.Count;
+            //
+
+            for (int i = 0; i < numFields; i++)
+            {
+                //Render each sub component according to its type
+                string currentField = block.typeFieldList[i];
+                //Todo: switch is case sensitive-- decide if we want case sensitivity among type names, get list of valid type names
+                switch (currentField)
+                {
+                    case "String":
+                        //Render text in this partition
+                        break;
+                    case "Block":
+                        //Create a new BlockControl for the subBlock and have it handle its own rendering. Just provide a space to render in
+                        break;
+                    case "Condition":   //?
+                        //Render conditional here. Enforce type restriction elsewhere. 
+                        break;
+                    case "DropMenu":
+                        //Render dropdown menu here-- user can select multiple values
+                        break;
+                    case "Textbox":
+                        //Render textbox here-- user can enter in values
+                        break;
+
+                    default:
+                        //Unknown / Missing type definition. Log and throw an error.
+                        break;
+                }
+            }
+
+            //Any final rendering code
+
+        }
+
+        private void InitializeComponent()
+        {
+            //throw new NotImplementedException();
+        }
+
+    }
+}
